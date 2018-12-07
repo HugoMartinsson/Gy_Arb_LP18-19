9f2848b34c274ec64fb6d3d9b7fa451382d39e80
HugoMartinsson <hugo.martinsson01@gmail.com>
2018-12-07 09:42:39 +0100

    Implementerat loginscriptet

---
+++ beta 1.0.1/betamobilelogin_1.0.1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Login_1.0.1</title>
+<link rel="stylesheet" href="betamobilecss_1.0.1.css" type="text/css">
+</head>
+
+<body>
+	<div id="wrapperlogin">
+    	<header>
+        	<h1 id="headerlogoh1">"LOGGA"</h1>
+        </header>
+        <section id="loginsection">
+        	<div id="loginsectionborder">
+                <div id="logoncredentialsdiv">
+                <form action="betamobile_1.0.1.php" method="get">
+                    <p id="logoncredentialsp">Användarnamn:</p>
+                    <input type="text" name="username" id="logoncredentialsfillin">
+                </div>
+                <div id="logoncredentialsdiv">
+                    <p id="logoncredentialsp">Lösenord:</p>
+                    <input type="text" name="password" id="logoncredentialsfillin">
+                </div>
+                <div id="logonbuttondiv">
+                	<input type="submit" value="Logga in" id="presstologin">
+                </div>
+                </form>
+            </div>
+        </section>
+    </div>
+</body>
+</html>
