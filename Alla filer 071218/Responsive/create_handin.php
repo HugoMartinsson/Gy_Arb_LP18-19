f293f37d5aa145eae11b258258dd2f6b709dac4f
JoakimMalmgren <43168103+JoakimMalmgren@users.noreply.github.com>
2019-01-11 11:58:10 +0100

    creathandin, css samt bilder

--- Alla filer 071218/Responsive/create_handin.php
+++ Alla filer 071218/Responsive/create_handin.php
@@ -15,39 +15,59 @@ session_start();
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>Namnlöst dokument</title>
+<title>Kurser</title>
+<link rel="stylesheet" href="styler.css" type="text/css">
 </head>
 
 <body>
-<form action="" method="get">	
-	<select name="course">
-        	<?php
-			//Skriver ut alla kurser så läraren får välja vart filen ska publiceras
-			foreach($result as $row)
-			{
+	<div id="wrapper">
+    	<header>
+        	<h1 id="headerlogoh1">"LOGGA"</h1>
+        </header>
+        <nav>
+        	<div id="dropdown">
+                <button onclick="myFunction()" class="dropbtn"></button>
+                    <div id="myDropdown" class="dropdown-content">
+                        <a href="start.php">Hem</a>
+                        <a href="kurser_overview_KLAR.php">Kurser/Klassrum</a>
+                        <a href="dropdowncontent">Länksamlingar</a>
+                        <a href="inlamning.php">Inlämningar</a>
+                        <a href="mittkonto.php">Mitt Konto</a>
+                    </div>
+             </div>
+        </nav>
+        <section>
+        	<div id="createhandindiv">
+				<form action="" method="get">	
+					<select name="course">
+        				<?php
+						//Skriver ut alla kurser så läraren får välja vart filen ska publiceras
+						foreach($result as $row)
+						{
+							?>
+        	 					<option value="<?php echo $row->Name; ?>"><?php echo $row->Name; ?></option>
+            				<?php
+						}
+						?>
+    			</select>
+    			<p id="hanndinp">Namn på inlämningen</p>
+				<input id="inputhandin" type="text" name="handInName"><br>
+    			<input id="inputhandin" type="submit">
+				</form>
+				<?php 
+					if(!empty($_GET))
+					{
+						$course = $_GET['course'];
+						$handInName = $_GET['handInName'];
+		
+						$sql = "INSERT INTO handin (HandInName, HandInCourse) VALUES (:handinname, :handincourse)";
+						$stmt = $dbh->prepare($sql);
+						$stmt->bindParam(":handinname", $handInName);
+						$stmt->bindParam(":handincourse", $course);
+						$stmt->execute();
+					}
 				?>
-        	 	<option value="<?php echo $row->Name; ?>"><?php echo $row->Name; ?></option>
-            	<?php
-			}
-			?>
-    </select>
-    <p>Namn på inlämningen</p>
-	<input type="text" name="handInName">
-    <input type="submit">
-</form>
-<?php 
-	if(!empty($_GET))
-	{
-		$course = $_GET['course'];
-		$handInName = $_GET['handInName'];
-		
-		$sql = "INSERT INTO handin (HandInName, HandInCourse) VALUES (:handinname, :handincourse)";
-		$stmt = $dbh->prepare($sql);
-		$stmt->bindParam(":handinname", $handInName);
-		$stmt->bindParam(":handincourse", $course);
-		$stmt->execute();
-		
-	}
-?>
+    	</div>
+	</section>
 </body>
 </html>
